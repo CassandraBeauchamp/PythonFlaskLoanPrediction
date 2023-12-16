dad45267142741ae15cbccafd7a59a450a1fbbd0
Cassandra Beauchamp <cassiewithstars@gmail.com>
2023-12-15 22:06:49 -0600

    Added basic pages for the website with nav bar

--- antonLending/main.py
+++ antonLending/main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+# Main
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask
+from flask import Flask, render_template, url_for, redirect
 
+app = Flask(__name__)
+@app.route("/")  # this sets the route to this page
+def home():
+	return render_template("home.html")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route("/AboutUs")  # this sets the route to this page
+def about():
+	return render_template("AboutUs.html")
 
+@app.route("/Statistics")  # this sets the route to this page
+def stat():
+	return render_template("Statistics.html")
+
+@app.route("/Staff")  # this sets the route to this page
+def staff():
+	return render_template("Staff.html")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=True)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
